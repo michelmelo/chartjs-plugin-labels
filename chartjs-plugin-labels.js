075b87f8390e4b1e7e729ce484b02c2f6ad36bb1
Michel Melo <michelmelo@users.noreply.github.com>
2020-09-14 11:43:08 +0100

    Create chartjs-plugin-labels.js

---
+++ chartjs-plugin-labels.js
@@ -0,0 +1,8 @@
+/**
+ * [chartjs-plugin-labels]{@link https://github.com/michelmelo/chartjs-plugin-labels}
+ *
+ * @version 0.1.0
+ * @author Michel Melo [michellander@gmail.com]
+ * @copyright Michel Melo 2020
+ * @license MIT
+ */
